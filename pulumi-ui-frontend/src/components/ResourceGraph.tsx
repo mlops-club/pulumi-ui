14e858ab0eea3499431a9b8ce55e026c3655a61c
phitoduck <eric.riddoch@gmail.com>
2024-10-04 19:50:11 -0600

    added autolayout to parents and children

--- pulumi-ui-frontend/src/components/ResourceGraph.tsx
+++ pulumi-ui-frontend/src/components/ResourceGraph.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import dagre from 'dagre';
 import {
     ReactFlow,
     Node,
@@ -8,6 +9,7 @@ import {
     MiniMap,
     useNodesState,
     useEdgesState,
+    Position,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { Resource } from '../types';
@@ -16,47 +18,64 @@ interface ResourceGraphProps {
     resources: Resource[];
 }
 
+const nodeWidth = 200;
+const nodeHeight = 50;
+
+const getLayoutedElements = (nodes: Node[], edges: Edge[], direction = 'LR') => {
+    const dagreGraph = new dagre.graphlib.Graph();
+    dagreGraph.setDefaultEdgeLabel(() => ({}));
+    dagreGraph.setGraph({ rankdir: direction });
+
+    nodes.forEach((node) => {
+        dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+    });
+
+    edges.forEach((edge) => {
+        dagreGraph.setEdge(edge.source, edge.target);
+    });
+
+    dagre.layout(dagreGraph);
+
+    const layoutedNodes = nodes.map((node) => {
+        const nodeWithPosition = dagreGraph.node(node.id);
+        return {
+            ...node,
+            position: {
+                x: nodeWithPosition.x - nodeWidth / 2,
+                y: nodeWithPosition.y - nodeHeight / 2,
+            },
+            targetPosition: Position.Left,
+            sourcePosition: Position.Right,
+        };
+    });
+
+    return { nodes: layoutedNodes, edges };
+};
+
 const ResourceGraph: React.FC<ResourceGraphProps> = ({ resources }) => {
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
     useMemo(() => {
-        const newNodes: Node[] = [];
-        const newEdges: Edge[] = [];
-        const nodeMap = new Map<string, Node>();
+        const newNodes: Node[] = resources.map((resource) => ({
+            id: resource.urn,
+            data: { label: `${resource.type}\n${resource.urn.split('::').pop()}` },
+            position: { x: 0, y: 0 }, // Initial position, will be updated by dagre
+        }));
 
-        resources.forEach((resource, index) => {
-            const node: Node = {
-                id: resource.urn,
-                data: { label: `${resource.type}\n${resource.urn.split('::').pop()}` },
-                position: { x: index * 200, y: index * 100 },
-            };
-            newNodes.push(node);
-            nodeMap.set(resource.urn, node);
+        const newEdges: Edge[] = resources
+            .filter((resource) => resource.parent)
+            .map((resource) => ({
+                id: `${resource.parent}-${resource.urn}`,
+                source: resource.parent!,
+                target: resource.urn,
+                type: 'smoothstep',
+            }));
 
-            if (resource.parent) {
-                newEdges.push({
-                    id: `${resource.parent}-${resource.urn}`,
-                    source: resource.parent,
-                    target: resource.urn,
-                });
-            }
-        });
+        const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(newNodes, newEdges);
 
-        // Adjust node positions based on parent-child relationships
-        newNodes.forEach((node) => {
-            const resource = resources.find((r) => r.urn === node.id);
-            if (resource && resource.parent) {
-                const parentNode = nodeMap.get(resource.parent);
-                if (parentNode) {
-                    node.position.x = parentNode.position.x + 200;
-                    node.position.y = parentNode.position.y + 100;
-                }
-            }
-        });
-
-        setNodes(newNodes);
-        setEdges(newEdges);
+        setNodes(layoutedNodes);
+        setEdges(layoutedEdges);
     }, [resources, setNodes, setEdges]);
 
     return (
@@ -66,6 +85,7 @@ const ResourceGraph: React.FC<ResourceGraphProps> = ({ resources }) => {
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
             fitView
+            attributionPosition="bottom-left"
         >
             <Background />
             <Controls />
